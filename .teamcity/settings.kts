e8dfc1114f888a8fad6e3bb0d4099372f2f70afb
e-e-m-i-l-e-e <emilyyy0621@gmail.com>
2026-07-29 13:32:22 +0300

    checkpoint

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,49 +1,14 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import _Self.*
 
 version = "2026.1"
 
 project {
-    buildType(Pre_Build_Dependencies)
-    buildType(Build_2)
+    buildType(Build)
 }
 
-object Pre_Build_Dependencies : BuildType({
-    name = "Pre-Build Dependencies"
-
-    params {
-        password("artifactory.api.key", "credentialsJSON:2811189a-9381-40ae-b4f7-e33e32bd5784")
-        param("artifactory.user", "admin")
-    }
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        installRequiredPackages()
-        setupPythonVenv()
-        setupConan()
-        script {
-            name = "Build And Push Conan Dependencies"
-            id = "Build_And_Push_Conan_Dependencies"
-            scriptContent = """
-                set -e
-                . .python/venv/bin/activate
-                conan install . --build=missing --output-folder=.conan -o app=Chrysalis -s build_type=Debug -pr:h linux -pr:b linux
-                conan upload "*" -r chrysalis-conan --confirm
-            """.trimIndent()
-        }
-    }
-
-    features {
-        perfmon {}
-    }
-})
-
-object Build_2 : BuildType({
+object Build : BuildType({
     name = "Build"
 
     params {
@@ -56,16 +21,48 @@ object Build_2 : BuildType({
     }
 
     steps {
-        installRequiredPackages()
-        setupPythonVenv()
-        setupConan()
         script {
-            name = "Conan Install"
-            id = "Conan_Install"
+            name = "Install Required Packages"
+            id = "Install_Required_Packages"
+            scriptContent = """
+                sudo apt update
+                sudo apt install -y cmake build-essential pkg-config
+            """.trimIndent()
+        }
+        script {
+            name = "Setup Python Virtual Environment"
+            id = "Setup_Python_Virtual_Environment"
             scriptContent = """
                 set -e
-                . .python/venv/bin/activate
-                conan install . --output-folder=.conan -o app=Chrysalis -s build_type=Debug -pr:h linux -pr:b linux
+                python3 -m venv .python/venv-linux
+                . .python/venv-linux/bin/activate
+                python -m pip install --upgrade pip
+                pip install -r requirements.txt
+            """.trimIndent()
+        }
+        script {
+            name = "Setup Conan"
+            id = "Setup_Conan"
+            scriptContent = """
+                set -e
+                . .python/venv-linux/bin/activate
+                conan remote add chrysalis-conan https://artifactory.lab.eemilee.me/artifactory/api/conan/chrysalis-conan --force
+                conan remote login chrysalis-conan %artifactory.user% -p %artifactory.api.key%
+                conan export .recipes/polyhook2
+                conan export .recipes/clo-sdk
+                conan download "qt/5.15.16" -r conancenter --only-recipe
+                conan download "qt/6.8.3" -r conancenter --only-recipe
+                git restore .conan/p
+            """.trimIndent()
+        }
+        script {
+            name = "Conan Install And Push Dependencies"
+            id = "Conan_Install_And_Push_Dependencies"
+            scriptContent = """
+                set -e
+                . .python/venv-linux/bin/activate
+                conan install . --build=missing --output-folder=.conan -o app=Chrysalis -s build_type=Debug -pr:h linux -pr:b linux -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+                conan upload "*" -r chrysalis-conan --confirm
             """.trimIndent()
         }
         script {
@@ -89,11 +86,5 @@ object Build_2 : BuildType({
 
     features {
         perfmon {}
-    }
-
-    dependencies {
-        snapshot(Pre_Build_Dependencies) {
-            onDependencyFailure = FailureAction.FAIL_TO_START
-        }
     }
 })
