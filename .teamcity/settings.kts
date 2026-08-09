02a1a99c2323e95e836a7e7e9d326d8cd88f35e1
e-e-m-i-l-e-e <emilyyy0621@gmail.com>
2026-08-09 22:39:12 +0300

    checkpoint

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -107,8 +107,8 @@ object Build : BuildType({
             id = "Upload_Documentation"
             scriptContent = """
                 set -ex
-                cd .conan/build/Debug/docs
-                tar -czvf documentation.tar.gz --exclude=documentation.tar.gz .
+                cd .conan/build/Debug
+                tar -czvf documentation.tar.gz docs
                 curl --fail-with-body -X POST \
                   -H "Authorization: Bearer %filebrowser.api.key%" \
                   --data-binary @documentation.tar.gz \
