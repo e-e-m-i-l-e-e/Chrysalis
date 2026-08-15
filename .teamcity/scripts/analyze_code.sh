2cdc808c337ae49017bba53c84c63520eae575f0
e-e-m-i-l-e-e <emilyyy0621@gmail.com>
2026-08-15 14:31:59 +0300

    checkpoint

--- .teamcity/scripts/analyze_code.sh
+++ .teamcity/scripts/analyze_code.sh
@@ -5,7 +5,7 @@ BUILD_TYPE="${1:?Build type is required}"
 
 . "$PYTHON_VENV"/bin/activate
 
-CodeChecker analyze .build/"$BUILD_TYPE"/compile_commands.json --analyzers clang-tidy --output .build/"$BUILD_TYPE"/codechecker --file=*/pattern* -j 4
+CodeChecker analyze .build/"$BUILD_TYPE"/compile_commands.json --analyzers clang-tidy --output .build/"$BUILD_TYPE"/codechecker -j 4
 set +e
 CodeChecker parse .build/"$BUILD_TYPE"/codechecker --export html --output .build/"$BUILD_TYPE"/docs/codechecker
 STATUS=$?
